764295dfb8033a196905d343e7e2e506dd2e14e0
Etim97 <wisdomlds97@gmail.com>
2025-11-20 12:18:35 +0100

    changesweek02

--- swagger.js
+++ swagger.js
@@ -6,11 +6,11 @@ const doc = {
     title: 'Contacts API',
     description: 'API for managing contacts',
   },
-  host: 'cse341-api.onrender.com', // Replace with your Render domain
-  schemes: ['https'],
+  host: 'cse341-2-db00.onrender.com',
+  schemes: ['http'],
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/contacts.js'];
+const outputFile = './swagger.json'; // This will be auto-generated
+const endpointsFiles = ['./routes/contacts.js']; // Your route file
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
